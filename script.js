e9fc04debfbaf05f623cbc5e1fad6a6622c4bfc5
Sandwich-God <reubensandwichclemens@gmail.com>
2021-03-07 23:01:06 -0500

    refining #1
    Removed eye in all images.  changed  the code to match with updated image names. Added feature so that you could see where the eyes are looking. When you click on the eyes it will replace that message with 'Ow!'.

--- script.js
+++ script.js
@@ -11,9 +11,16 @@ pic=''; //This is a Global Variable named pic, it will be used later on to defin
 //This function is named blink(). Its purpose is to change the src of the two images in the middle table to closed eyes for a brief amount of time.
 function blink() 
 {
-	document.getElementById("picture").src='images/eyeclosed.gif'; //Changes the src of the image that has the id, picture, to an image of eyes closed.
-	document.getElementById("picture2").src='images/eyeclosed.gif'; //Changes the src of the image that has the id, picture2, to an image of eyes closed.
+	document.getElementById("picture").src='images/closed.gif'; //Changes the src of the image that has the id, picture, to an image of eyes closed.
+	document.getElementById("picture2").src='images/closed.gif'; //Changes the src of the image that has the id, picture2, to an image of eyes closed.
 	setTimeout(open, 500); //Sets up timer event that will go to the open() function in 500 milliseconds.
+}
+
+//This function is named poked(). Its purpose is to change the image to make it look like they got poked.
+function poked(eyep)
+{
+	document.getElementById("direction").innerHTML = 'Ow!';
+	document.getElementById(eyep).src='images/closed.gif'; //Changes the src of the image that has the id, picture, to an image of eyes closed.
 }
 
 //This function is named open(). Its purpose is to change the src of the two images to whatever the onmouseover event has in its parameters, or eyes forward when page loads.
@@ -23,13 +30,9 @@ function open()
 	//the onmouseover event hasn't been activated yet, so the pic variable has not changed.
 	if (pic === '') //If pic equals null, activate.
 	{
-		document.getElementById("picture").src='images/eyeforward.gif'; //Changes the src of the image that has the id, picture, to an image of eyes looking forward.
-		document.getElementById("picture2").src='images/eyeforward.gif'; //Changes the src of the image that has the id, picture2, to an image of eyes looking forward.
-	}
-	if (pic == 'eyeforward')
-	{
-		document.getElementById("picture").src='images/eyeleft.gif';
-		document.getElementById("picture2").src='images/eyeright.gif';
+		document.getElementById("direction").innerHTML = 'foward';
+		document.getElementById("picture").src='images/forward.gif'; //Changes the src of the image that has the id, picture, to an image of eyes looking forward.
+		document.getElementById("picture2").src='images/forward.gif'; //Changes the src of the image that has the id, picture2, to an image of eyes looking forward.
 	}
 	//This else if statement will activate when you move onto the page. This is because the pic variable will change values as the onmouseover event will come into action.
 	else if (pic != '') //Else if pic is anything but null.
@@ -40,12 +43,12 @@ function open()
 	setTimeout(blink, 1000); //Sets up a timer event that will go to the blink() function in 1000 miliseconds/1 second.
 }
 
-
 //This function is named mouseover(). Its purpose is to trigger whenever the onmouseover event is activated. The parameter x will get whatever message the
 //specific onmouseover event has.
 function mouseover(x)
 {
 	pic = x; //Defines the pic variable by what x is from the parameter. The pic will return its value to the global variable pic.
+	document.getElementById("direction").innerHTML = x;
 	document.getElementById("picture").src='images/' + x + '.gif'; //Changes the src of the image that has the id, picture, to an image of whatever onmouseover event parameter is.
 	document.getElementById("picture2").src='images/' + x + '.gif'; //Changes the src of the image that has the id, picture2, to an image of whatever onmouseover event parameter is.
 }
